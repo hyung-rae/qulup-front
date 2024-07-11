88b6bfde4d09a43b6cf27f955b29b44ab2d385b9
우형래 <woohr1109@gmail.com>
2024-07-11 21:31:53 +0900

    🚧 Wip: header menu

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,20 @@
+import { BackgroundImage, Container, Text } from '@mantine/core'
+
 const Home = () => {
-  return <>Home</>
+  const lorem =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ullam, ex cum repellat alias ea nemo. Ducimus ex nesciunt hic ad saepe molestiae nobis necessitatibus laboriosam officia, reprehenderit, earum fugiat?'
+
+  return (
+    <Container>
+      {Array(40)
+        .fill(0)
+        .map((_, index) => (
+          <Text size="lg" key={index} my="md" maw={600} mx="auto" c={'white'}>
+            {lorem}
+          </Text>
+        ))}
+    </Container>
+  )
 }
 
 export default Home
