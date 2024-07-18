6172031aa341f0148dc92223c2ce2b9e510f0f92
우형래 <woohr1109@gmail.com>
2024-07-18 17:53:58 +0900

    🐛 Fix: next config

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     loader: 'akamai',
     path: '',
   },
-  assetPrefix: './',
+  assetPrefix: process.env.NEXT_PUBLIC_NODE_ENV === 'local' ? '/' : 'https://hyung-rae.github.io/qulup-front/',
 }
 
 export default nextConfig
