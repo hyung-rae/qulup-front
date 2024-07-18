2671fcc6701890016f9757ff824413507fbfb64f
우형래 <woohr1109@gmail.com>
2024-07-18 18:18:06 +0900

    🐛 Fix: next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://hyung-rae.github.io/qulup-front' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://hyung-rae.github.io/qulup-front/' : '/',
 }
 
 export default nextConfig
