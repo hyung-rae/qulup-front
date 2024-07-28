3676f4fd78f6f31a14e5440de5b2a931f1875f84
우형래 <woohr1109@gmail.com>
2024-07-28 21:54:25 +0900

    ✨ Feat: 헤더, 결제내역

--- src/components/layout/header/Header.presenter.jsx
+++ src/components/layout/header/Header.presenter.jsx
@@ -1,86 +1,80 @@
-import { ActionIcon, Button, Flex, Group, Image, Indicator, Text, Tooltip } from '@mantine/core'
+import { ActionIcon, Button, Flex, Group, Image, Indicator, Stack, Text, Tooltip } from '@mantine/core'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import classes from './Header.module.css'
 import { IconShoppingCart, IconUserCog, IconNews } from '@tabler/icons-react'
 
 const HeaderUI = ({ isLogin, onClickSignIn, handleLogOut }) => {
-  const { basePath } = useRouter()
+  const { basePath, pathname } = useRouter()
   return (
-    <Flex bg={'dark.7'} align={'center'} h={'100%'} w={'100%'} className={classes.header_container}>
-      <Link href={'/'}>
-        <Image radius="md" w={'auto'} h={20} src={`${basePath}/images/logo.png`} alt={'logo'} />
-      </Link>
+    <Stack justify="flex-end" h={'100%'} bg={'dark.7'} p={5} gap={10}>
+      <Group justify="space-around" align={'center'} className={classes.header_container}>
+        <Link href={'/'}>
+          <Image radius="md" w={'auto'} h={20} src={`${basePath}/images/logo.png`} alt={'logo'} />
+        </Link>
 
-      {/* 로그아웃 ui */}
-      {!isLogin && (
-        <Group gap={15}>
-          <Button size="compact-md" variant="transparent" className={classes.label} onClick={onClickSignIn}>
-            로그인
-          </Button>
-        </Group>
-      )}
-
-      {/* 로그인 ui */}
-      {isLogin && (
-        <Flex direction="column" gap={15} pt="sm" className={classes.user_section}>
+        {/* 로그아웃 ui */}
+        {!isLogin && (
           <Group gap={15}>
-            <Group gap={10}>
-              <Text fz="sm" fw="bolder" c="white">
-                우형래 님
-              </Text>
-              <Text fz="sm" fw="bolder" c="white">
-                280 P
-              </Text>
-            </Group>
+            <Button size="compact-md" variant="transparent" className={classes.label} onClick={onClickSignIn}>
+              로그인
+            </Button>
+          </Group>
+        )}
 
-            <Group gap={5}>
-              <Link href={'/service'}>
-                <Button color="dark" size="compact-xs">
-                  고객센터
-                </Button>
+        {/* 로그인 ui */}
+        {isLogin && (
+          <Group gap={15}>
+            <Text fz="sm" fw="bolder" c="white">
+              우형래 님
+            </Text>
+            <Text fz="sm" fw="bolder" c="white">
+              280 P
+            </Text>
+
+            <Indicator disabled>
+              <Link href={'/user/info'}>
+                <ActionIcon variant="transparent" color="dark.9" c="white" size="sm" radius="sm">
+                  <IconUserCog style={{ width: '80%', height: '80%' }} stroke={2} />
+                </ActionIcon>
               </Link>
-
-              <Button variant="default" size="compact-xs" onClick={handleLogOut}>
-                로그아웃
-              </Button>
-            </Group>
+            </Indicator>
+            <Indicator color="red" size={5} offset={4}>
+              <Link href={'/cart'}>
+                <ActionIcon variant="transparent" color="dark.9" c="white" size="sm" radius="sm">
+                  <IconShoppingCart style={{ width: '80%', height: '80%' }} stroke={2} />
+                </ActionIcon>
+              </Link>
+            </Indicator>
+            <Button size="compact-xs" onClick={handleLogOut}>
+              로그아웃
+            </Button>
           </Group>
-
-          <Group justify="flex-end" gap={10}>
-            <Tooltip label={<Text fz={10}>Math Gemstone</Text>} position="bottom" offset={0} color="dark.9">
-              <Indicator disabled>
-                <Link href={'/gemstone'}>
-                  <ActionIcon variant="transparent" color="dark.9" c="white" size="md" radius="md">
-                    <IconNews style={{ width: '80%', height: '80%' }} stroke={2} />
-                  </ActionIcon>
-                </Link>
-              </Indicator>
-            </Tooltip>
-
-            <Tooltip label={<Text fz={10}>마이페이지</Text>} position="bottom" offset={0} color="dark.9">
-              <Indicator disabled>
-                <Link href={'/user/info'}>
-                  <ActionIcon variant="transparent" color="dark.9" c="white" size="md" radius="md">
-                    <IconUserCog style={{ width: '80%', height: '80%' }} stroke={2} />
-                  </ActionIcon>
-                </Link>
-              </Indicator>
-            </Tooltip>
-
-            <Tooltip label={<Text fz={10}>장바구니</Text>} position="bottom" offset={0} color="dark.9">
-              <Indicator color="red" size={5} offset={4}>
-                <Link href={'/cart'}>
-                  <ActionIcon variant="transparent" color="dark.9" c="white" size="md" radius="md">
-                    <IconShoppingCart style={{ width: '80%', height: '80%' }} stroke={2} />
-                  </ActionIcon>
-                </Link>
-              </Indicator>
-            </Tooltip>
-          </Group>
-        </Flex>
-      )}
-    </Flex>
+        )}
+      </Group>
+      <Group justify="center">
+        <Link href={'/problems'}>
+          <Button variant="subtle" size="compact-sm" color={pathname === '/problems' ? 'teal' : 'white'}>
+            문제
+          </Button>
+        </Link>
+        <Link href={'/examProblems'}>
+          <Button variant="subtle" size="compact-sm" color={pathname === '/examProblems' ? 'teal' : 'white'}>
+            모의고사
+          </Button>
+        </Link>
+        <Link href={'/gemstone'}>
+          <Button variant="subtle" size="compact-sm" color={pathname === '/gemstone' ? 'teal' : 'white'}>
+            Math Gemstone
+          </Button>
+        </Link>
+        <Link href={'/service'}>
+          <Button variant="subtle" size="compact-sm" color={pathname === '/service' ? 'teal' : 'white'}>
+            고객센터
+          </Button>
+        </Link>
+      </Group>
+    </Stack>
   )
 }
 
--- src/components/user/payment/Payment.presenter.jsx
+++ src/components/user/payment/Payment.presenter.jsx
@@ -1,55 +1,88 @@
-import { Box, Button, Table, Text } from '@mantine/core'
+import { Box, Button, Group, Stack, Table, Text, Tooltip } from '@mantine/core'
+import classes from './Payment.module.css'
+import Link from 'next/link'
 
 const PaymentUI = () => {
   return (
     <Box>
-      <Table.ScrollContainer minWidth={600} h={'80vh'} type="native" p={30}>
-        <Table>
+      <Table.ScrollContainer lin minWidth={600} h={'80vh'} type="native" p={30}>
+        <Table className={classes.payment_table}>
           <Table.Thead>
             <Table.Tr>
-              <Table.Th>주문번호</Table.Th>
+              <Table.Th>결제상품</Table.Th>
               <Table.Th>결제유형</Table.Th>
               <Table.Th>결제금액</Table.Th>
               <Table.Th>결제상태</Table.Th>
               <Table.Th>결제일</Table.Th>
-              <Table.Th>환불신청</Table.Th>
               <Table.Th>영수증</Table.Th>
             </Table.Tr>
           </Table.Thead>
           <Table.Tbody>
             <Table.Tr>
-              <Table.Td>241234-2402012</Table.Td>
+              <Table.Td>
+                <Stack gap={5}>
+                  <Group justify="space-between">
+                    <Text size="sm" fw={700}>
+                      확률과 통계 & 미적분
+                    </Text>
+                    <Button size="compact-xs" color="dark">
+                      다운로드
+                    </Button>
+                  </Group>
+                  <Group justify="space-between">
+                    <Text size="sm" fw={700}>
+                      확률과 통계 & 미적분
+                    </Text>
+                    <Tooltip label="24.04.12 까지 다운로드 가능" size="xs">
+                      <Button size="compact-xs" color="dark">
+                        다운로드
+                      </Button>
+                    </Tooltip>
+                  </Group>
+                </Stack>
+              </Table.Td>
               <Table.Td>신용카드</Table.Td>
-              <Table.Td>30,000 원</Table.Td>
+              <Table.Td>72,000 원</Table.Td>
               <Table.Td>결제완료</Table.Td>
               <Table.Td>24.11.02</Table.Td>
               <Table.Td>
-                <Button fullWidth size="compact-xs" color="dark">
-                  신청
-                </Button>
-              </Table.Td>
-              <Table.Td>
-                <Text size="xs" component="a" td={'underline'}>
-                  영수증 보기
-                </Text>
+                <Link href={'#'}>
+                  <Text size="xs">영수증 보기</Text>
+                </Link>
               </Table.Td>
             </Table.Tr>
 
             <Table.Tr>
-              <Table.Td>241234-2402012</Table.Td>
+              <Table.Td>
+                <Stack gap={5}>
+                  <Group justify="space-between">
+                    <Text size="sm" fw={700}>
+                      확률과 통계 & 미적분
+                    </Text>
+                    <Button size="compact-xs" color="dark">
+                      다운로드
+                    </Button>
+                  </Group>
+                  <Group justify="space-between">
+                    <Text size="sm" fw={700}>
+                      확률과 통계 & 미적분
+                    </Text>
+                    <Tooltip label="24.04.12 까지 다운로드 가능" size="xs">
+                      <Button size="compact-xs" color="dark">
+                        다운로드
+                      </Button>
+                    </Tooltip>
+                  </Group>
+                </Stack>
+              </Table.Td>
               <Table.Td>신용카드</Table.Td>
-              <Table.Td>30,000 원</Table.Td>
+              <Table.Td>72,000 원</Table.Td>
               <Table.Td>결제완료</Table.Td>
               <Table.Td>24.11.02</Table.Td>
               <Table.Td>
-                <Button fullWidth size="compact-xs" color="dark">
-                  신청
-                </Button>
-              </Table.Td>
-              <Table.Td>
-                <Text size="xs" component="a" td={'underline'}>
-                  영수증 보기
-                </Text>
+                <Link href={'#'}>
+                  <Text size="xs">영수증 보기</Text>
+                </Link>
               </Table.Td>
             </Table.Tr>
           </Table.Tbody>
