89aa54924df3f86f35262036b30d76255a1e9efa
jeongbeen2 <aceman9508@gmail.com>
2024-08-06 20:53:57 +0900

    wip: 배포환경 쿠키 테스트

--- pages/api/signIn.js
+++ pages/api/signIn.js
@@ -1,10 +1,31 @@
 import axios from 'axios'
+const DEFAULT_URL = 'http://3.35.131.46/'
 
-const DEFAULT_URL = 'http://3.35.131.46/QV1'
+const axiosInstance = axios.create({
+  baseURL: `${DEFAULT_URL}`,
+  headers: {
+    'Content-Type': 'multipart/form-data',
+  },
+})
+
+// 인터셉터 설정
+axiosInstance.interceptors.request.use(
+  config => {
+    const cookies = document.cookie // 브라우저의 쿠키를 가져옴
+    console.log('cookies1:', cookies)
+    config.headers.Cookie = cookies // 요청 헤더에 쿠키를 추가
+    console.log('cookies2:', config.headers)
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  },
+)
+
 export const postSignUp = async params => {
   console.log('API TODO: 데이터를 param으로 받아서, 데이터를 보내주세요.')
   try {
-    const res = await axios.post(`${DEFAULT_URL}/createMember.do`, params)
+    const res = await axiosInstance.post(`${DEFAULT_URL}QV1/createMember.do`, params)
     console.log('params: ', params)
     console.log('res: ', res)
     return res.data
@@ -16,7 +37,15 @@ export const postSignUp = async params => {
 
 export const postVerificationEmail = async params => {
   try {
-    const res = await axios.post(`${DEFAULT_URL}/verificationEmail.do`, params)
+    const formData = new FormData()
+    for (const key in params) {
+      if (params.hasOwnProperty(key)) {
+        formData.append(key, params[key])
+      }
+    }
+
+    const res = await axiosInstance.post('QV1/verificationEmail.do', formData)
+
     console.log('이메일인증 param: ', params)
     console.log('이메일인증 응답: ', res)
     return res.data
@@ -28,7 +57,14 @@ export const postVerificationEmail = async params => {
 
 export const postVerificationPhone = async params => {
   try {
-    const res = await axios.post(`${DEFAULT_URL}/sendVerificationPhone.do`, params)
+    const formData = new FormData()
+    for (const key in params) {
+      if (params.hasOwnProperty(key)) {
+        formData.append(key, params[key])
+      }
+    }
+
+    const res = await axiosInstance.post(`${DEFAULT_URL}QV1/sendVerificationPhone.do`, formData)
     console.log('핸드폰인증 params:', params)
     console.log('핸드폰인증 응답: ', res)
     return res.data
--- src/components/examProblems/ExamProblems.jsx
+++ src/components/examProblems/ExamProblems.jsx
@@ -1,7 +1,14 @@
-import { Box, Container, Flex, Group, SegmentedControl, Text } from '@mantine/core'
+import { Box, Container, Flex, Group, MultiSelect, SegmentedControl, Select, Text, TextInput } from '@mantine/core'
 import ExamProblemsItem from './ExamProblemsItem'
-
+import { ACADEMY_FILTER, PROBLEM_FILTER, SEARCH_FILTER } from '@/src/components/problems/SelectFilter'
+import { useState } from 'react'
 const Gemstone = () => {
+  const [placeholder, setPlaceholder] = useState({
+    academy: true,
+    problem: true,
+  })
+  const [searchText, setSearchText] = useState('')
+
   return (
     <Container bg="gray.3" maw={1700} fluid p="xl" mih={'100vh'}>
       <Flex direction="column" m="auto" gap={20}>
@@ -9,14 +16,45 @@ const Gemstone = () => {
           <Text fz={26} fw={900} variant="gradient" gradient={{ from: 'gray', to: 'dark', deg: 0 }}>
             모의고사 문제
           </Text>
-          <SegmentedControl
-            size="md"
-            w={300}
-            withItemsBorders={false}
-            data={['Top', 'New', 'Greatest']}
-            mb={20}
-            color="dark"
-          />
+          <Group justify="flex-end" align="flex-end">
+            <MultiSelect
+              miw={200}
+              data={ACADEMY_FILTER}
+              placeholder={placeholder.academy ? '제외 학원' : ''}
+              onChange={e => {
+                setPlaceholder(prev => {
+                  return { ...prev, academy: !(e.length > 0) }
+                })
+              }}
+            />
+            <MultiSelect
+              miw={200}
+              placeholder={placeholder.problem ? '제외 단원' : ''}
+              data={PROBLEM_FILTER}
+              onChange={e => {
+                setPlaceholder(prev => {
+                  return { ...prev, problem: !(e.length > 0) }
+                })
+              }}
+              comboboxProps={{ width: 200, position: 'bottom-end' }}
+            />
+            <Group gap={5} align="flex-end">
+              <Select
+                placeholder="검색타입"
+                data={SEARCH_FILTER}
+                w={100}
+                defaultValue="all"
+                allowDeselect={false}
+                withCheckIcon={false}
+              />
+              <TextInput
+                value={searchText}
+                placeholder="검색어를 입력해주세요"
+                onChange={e => setSearchText(e.target.value)}
+                rightSection={<IconSearch width={16} height={16} onClick={handleSearch} cursor={'pointer'} />}
+              />
+            </Group>
+          </Group>
         </Group>
         <ExamProblemsItem />
         <ExamProblemsItem />
